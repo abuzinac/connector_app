3036d415c8dd11107fe560a51b3fc8fe943769b6
Andrija Buzinac <andrija.buzinac@gmail.com>
2022-08-12 23:53:30 +0200

    Start working on social network... Startin register page...

---
+++ register.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to Connector</title>
+</head>
+<body>
+
+<form action="register.php" method="post">
+
+    <label> <!--  test subject-->
+        <input type="text" name="reg_fname" placeholder="First Name" required>
+        <br>
+        <input type="text" name="reg_lname" placeholder="Last Name" required>
+        <br>
+        <input type="email" name="reg_email" placeholder="Email" required>
+        <br>
+        <input type="email" name="reg_email2" placeholder="Confirm Email" required>
+        <br>
+        <input type="password" name="reg_password" placeholder="Password" required>
+        <br>
+        <input type="password" name="reg_password2" placeholder="Confirm Password" required>
+        <br>
+        <input type="submit" name="register_button" value="Register">
+    </label>
+</form>
+
+</body>
+</html>
