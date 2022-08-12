55a2a4826296a2415d14c9651d90ad9329ecc469
Andrija Buzinac <andrija.buzinac@gmail.com>
2022-08-13 00:22:54 +0200

    Testing commit on php storm...

--- register.php
+++ register.php
@@ -1,3 +1,70 @@
+<?php
+
+$con = mysqli_connect("localhost", "root", "", "connector");
+
+if (mysqli_connect_errno()) {
+    echo "Failed to connect: " . mysqli_connect_errno();
+}
+
+//Declaring variables to prevent errors
+$fname = ""; //First name
+$lname = ""; //Last name
+$em = ""; //Email
+$em2 = ""; //Email2
+$password = ""; //Password
+$password2 = ""; //Password2
+$date = ""; //Sign up date
+$error_array = ""; //Holds error messages
+
+if (isset($_POST['register_button'])) {
+
+    //Register form values
+
+    //First Name
+    $fname = strip_tags($_POST['reg_fname']); //Remove html tags
+    $fname = str_replace(' ', '', $fname); //Remove spaces
+    $fname = ucfirst(strtolower($fname)); //Uppercase first letter
+
+    //Last Name
+    $lname = strip_tags($_POST['reg_lname']); //Remove html tags
+    $lname = str_replace(' ', '', $lname); //Remove spaces
+    $lname = ucfirst(strtolower($lname)); //Uppercase first letter
+
+    //email
+    $em = strip_tags($_POST['reg_email']); //Remove html tags
+    $em = str_replace(' ', '', $em); //Remove spaces
+    $em = ucfirst(strtolower($em)); //Uppercase first letter
+
+    //email 2
+    $em2 = strip_tags($_POST['reg_email2']); //Remove html tags
+    $em2 = str_replace(' ', '', $em2); //Remove spaces
+    $em2 = ucfirst(strtolower($em2)); //Uppercase first letter
+
+    //password
+    $password = strip_tags($_POST['reg_password']); //Remove html
+    //password2
+    $password2 = strip_tags($_POST['reg_password2']); //Remove html tags
+
+    //current date
+    $date = date("d-m-Y"); //todo date format date(Y-m-d)
+
+    if ($em == $em2) {
+        //Check if email is in valid format
+        if (filter_var($em, FILTER_VALIDATE_EMAIL)) {
+            $em = filter_var($em, FILTER_VALIDATE_EMAIL);
+        } else {
+            echo "Invalid format";
+        }
+
+    } else {
+        echo "Emails don't match";
+    }
+
+}
+
+?>
+
+
 <!doctype html>
 <html lang="en">
 <head>
